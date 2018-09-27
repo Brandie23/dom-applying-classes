87c93bdcceaf5d185e7e7d35f431152d1f17dbd5
unknown <brandie23ross@hotmail.com>
2018-09-27 17:08:03 +1200

    [5.2 finished]

--- classes.js
+++ classes.js
@@ -8,7 +8,7 @@ function start () {
    one()
    two()
    three()
-    
+    makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -33,3 +33,7 @@ function three () {
 }
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+ var makeVisible = document.getElementsByClassName('invisible')[0]
+ makeVisible.classList.add('visible')
+}
